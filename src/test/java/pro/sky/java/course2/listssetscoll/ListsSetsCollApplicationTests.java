c7ab923442a232597b14c6acf707a68eeee849fd
Admin <anstdor@gmail.com>
2023-04-15 14:05:58 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/listssetscoll/ListsSetsCollApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.listssetscoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsSetsCollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
